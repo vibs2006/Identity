db18df5a078475660474c5ce92573fba54997d60
Vibs <Vibs@DESKTOP-12124VN>
2018-03-04 22:03:07 +0530

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            var testMain = MainThread("1");
+            Console.WriteLine("Cursor Moved");
+            var testMain2 = MainThread("2");
+            Task.WaitAll(testMain,testMain2);
+            Console.WriteLine("All Conditions Completed");
+            Console.ReadLine();
+        }
+
+        private static async Task<decimal> MainThread(string taskno)
+        {
+            var task = longCalculation();
+            Console.WriteLine("Other Tasks Independently Started");
+            var a = await task;
+            Console.WriteLine($"Task {taskno} from longCalculation Completed");
+            return a;          
+        }
+
+        private static async Task<decimal> longCalculation()
+        {
+            Console.WriteLine("Long Calculation Stated");
+            await Task.Delay(3000);
+            Console.WriteLine("Long Calculation Ended");
+            return 1;            
+        }
+    }
+}
---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [customAuthorize]
+    public class testController : Controller
+    {
+        // GET: test
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Authenticated()
+        {
+            var identity = Thread.CurrentPrincipal.IsInRole("admin");
+            
+            return View(identity);
+        }
+    }
+
+    public class customAuthorizeAttribute
+        : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+              Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity("vibs"), new string[] {"admin" }
+               );
+            //base.OnAuthorization(filterContext);
+        }
+    }
+}
